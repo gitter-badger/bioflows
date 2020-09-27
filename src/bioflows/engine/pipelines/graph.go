0d54cd5cbc8a0e9a2550d352a1e350d06984bc29
Mohamed Fawzy <mfawzy.sami@gmail.com>
2020-09-27 12:28:44 +0300

    Start adding Graph

--- src/bioflows/engine/pipelines/graph.go
+++ src/bioflows/engine/pipelines/graph.go
@@ -1,3 +1,8 @@
 package pipelines
 
-
+func PreparePipeline(b *BioPipeline) (*BioPipeline,error) {
+	//TODO: this function should perform the following tasks
+	// 1. Download the tool from the remote repository, in this order (URL,Bioflows Hub)
+	// 2. Update the downloaded tool parameters by the newly written parameters.
+	return b , nil
+}
