0e22ca6a0e17cda32104f7693aca863017268f11
Mohamed Fawzy <mfawzy.sami@gmail.com>
2020-10-07 09:51:40 +0300

    Adding Download functionality in all the command line options

--- src/bioflows/cli/dag.go
+++ src/bioflows/cli/dag.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"bioflows/helpers"
 	"bioflows/models/pipelines"
 	"fmt"
 	"gopkg.in/yaml.v2"
@@ -10,22 +11,32 @@ import (
 
 func RenderGraphViz(pipelineFile string) (string,error) {
 	pipeline := &pipelines.BioPipeline{}
-	tool_in, err := os.Open(pipelineFile)
 
-	if err != nil {
-		fmt.Printf("There was an error opening the tool file, %v\n",err)
-		os.Exit(1)
-	}
-	mytool_content, err := ioutil.ReadAll(tool_in)
-	if err != nil {
-		fmt.Printf("Error reading the contents of the tool , %v\n",err)
-		os.Exit(1)
-	}
-	err = yaml.Unmarshal([]byte(mytool_content),pipeline)
-	if err != nil {
-		//fmt.Println("There was a problem unmarshaling the current tool")
-		fmt.Println(err.Error())
-		return "" , err
+	if !helpers.IsValidUrl(pipelineFile) {
+		tool_in, err := os.Open(pipelineFile)
+
+		if err != nil {
+			fmt.Printf("There was an error opening the tool file, %v\n",err)
+			os.Exit(1)
+		}
+		mytool_content, err := ioutil.ReadAll(tool_in)
+		if err != nil {
+			fmt.Printf("Error reading the contents of the tool , %v\n",err)
+			os.Exit(1)
+		}
+		err = yaml.Unmarshal([]byte(mytool_content),pipeline)
+		if err != nil {
+			//fmt.Println("There was a problem unmarshaling the current tool")
+			fmt.Println(err.Error())
+			return "" , err
+		}
+	}else{
+		// that means the file is remote
+		err :=helpers.DownloadBioFlowFile(pipeline,pipelineFile)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Error Downloading the file: %s",err.Error()))
+			return "", err
+		}
 	}
 	b , err := pipelines.PreparePipeline(pipeline,nil)
 	if err != nil {
