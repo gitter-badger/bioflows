c8da260334f488cbac65634677cb21a925dfed18
Mohamed Fawzy <mfawzy.sami@gmail.com>
2021-02-16 11:17:29 +0300

    Correcting the version

--- src/bf/cmd/root.go
+++ src/bf/cmd/root.go
@@ -38,7 +38,7 @@ var rootCmd = &cobra.Command{
 	Use:   "bf",
 	Short: "Distributed Containerized Bioinformatics and Computational Workflows/Pipeline engine built in Golang",
 	Long: `BioFlows is a distributed pipeline framework for expressing , designing and running scalable reproducible and distributed computational bioinformatics workflows in cloud containers. BioFlows Framework consists of software tools and cloud microservices that communicate together to achieve a highly distributed , highly coordinated and fault tolerant environment to run parallel bioinformatics pipelines onto cloud containers and cloud servers. BioFlows also has BioFlows Description Language (BDL) which is an imperative and declarative standard for describing and expressing computational bioinformatics tools and pipelines, BDL is flexible , easy to use and a human readable language that enables researchers to design reproducible and scalable computational pipelines.The language is based entirely on Yet Another Markup Language (YAML).`,
-	Version: "0.0.2a",
+	Version: "0.0.3a",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
